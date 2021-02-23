f293bbc29bb4a2441e9bf4729355a64ea4197bf1
Aleena <apsshrestha28>
2021-02-22 20:02:24 -0800

    migration file renamed

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_070726) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "customer_id", null: false
+    t.bigint "customer_id"
     t.index ["customer_id"], name: "index_reviews_on_customer_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
