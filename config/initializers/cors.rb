fada84777cbc5bf07ef04332796eb7f651e4a5b3
Aleena <apsshrestha28>
2021-03-03 15:08:00 -0800

    cors changed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # orgin acccepts an array of domain names. These are all the whitelisted DOMAINS that are allowed to make CORS request
-    origins 'localhost:5505'
+    origins '*'
     resource(
-      "/api/*", # only allows CORS requeswts to a path that looks like /api
+      "*", # only allows CORS requeswts to a path that looks like /api
       headers: :any, # allow requests to contain any headers
       credentials: true, # allows us to send cookies through CORS requests
       methods: [:get, :post, :delete, :patch, :put, :options]
