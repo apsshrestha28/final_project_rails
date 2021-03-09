fc5bd4595474c17874a8ffb1d5bfb59731608b1e
Aleena <apsshrestha28>
2021-03-09 01:28:07 -0800

    change for logging

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -8,11 +8,17 @@ class Api::ApplicationController < ApplicationController
   helper_method :user_signed_in?
 
   def current_user
+    puts "print session userid"
+    puts session[:user_id]
     if session[:user_id].present?
-    @current_user ||=User.find_by_id session[:user_id]
-    
+      @current_user ||=User.find_by_id session[:user_id]
+      puts @current_user
 
   elsif session[:customer_id].present?
+      puts "customer id session"
+      puts session[:customer_id]
+      puts "print current user"
+      puts @current_user
       @current_user ||=Customer.find_by_id session[:customer_id]
     end
   end
