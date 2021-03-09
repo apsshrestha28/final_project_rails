5b22f576f3b5261126c1ab92730eb9140a618444
Aleena <apsshrestha28>
2021-03-08 16:16:18 -0800

    cors changed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # orgin acccepts an array of domain names. These are all the whitelisted DOMAINS that are allowed to make CORS request
-    origins 'https://epic-lumiere-e33337.netlify.app/','http://localhost:5505'
+    origins 'https://epic-lumiere-e33337.netlify.app','http://localhost:5505'
     resource(
       "*", 
       headers: :any, # allow requests to contain any headers
