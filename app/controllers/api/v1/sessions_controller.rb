1f164c1a53820347a476c9f5993f3281313cd225
Aleena <apsshrestha28>
2021-03-10 12:56:11 -0800

    removed private

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -56,7 +56,7 @@ class Api::V1::SessionsController < Api::ApplicationController
   def get_current_user
     render json: current_user
   end
-  private
+
   def current_user
     puts "print session userid"
     puts session[:user_id]
