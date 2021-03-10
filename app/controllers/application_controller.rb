27ec0acf3396b85f6ce44150863a7f747103286c
Aleena <apsshrestha28>
2021-03-10 13:12:24 -0800

    changes for deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+ private
+  def current_user
+    puts "print session userid"
+    puts session[:user_id]
+    if session[:user_id].present?
+      @current_user ||=User.find_by_id session[:user_id]
 
+  elsif session[:customer_id].present?
+      puts "customer id session"
+      puts session[:customer_id]
+      puts "print current user"
+      @current_user ||=Customer.find_by_id session[:customer_id]
+    end
+  end
+  helper_method :current_user
 end
